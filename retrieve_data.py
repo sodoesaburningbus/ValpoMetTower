a9250e9903bccd4ade5a9292c5d2bbb3f0afc603
christopher phillips1 <cphill19@bergeron.valpo.edu>
2024-09-13 16:46:51 +0000

    Fixed dewpt. It's doesn't exist.

--- retrieve_data.py
+++ retrieve_data.py
@@ -6,13 +6,14 @@
 
 ### Import libraries
 from datetime import datetime
+import os
 import urllib.request as ureq
 
 ### The tower file url
-url = ''
+url = 'http://10.3.78.241/twidata.txt'
 
 ### location to save the data
-sdir = './'
+sdir = '/archive/campus_mesonet_data'
 
 # Pull the data and process it
 data = list(ureq.urlopen(url))[-1].decode('utf-8') # Retrieve text file and grab last line only
@@ -22,31 +23,37 @@ date = datetime.strptime(data[:17], "%y/%m/%d %H:%M:%S") # Local
 wdir = float(data[18:21]) # deg
 wspd = float(data[22:27])*0.447 # m/s
 sdown = float(data[32:36]) # W/m2
-dewp = (float(data[38:43])-32.0)/1.8 # 'C
 temp = (float(data[45:50])-32.0)/1.8 # 'C
 rh = float(data[52:55]) # %
 pres = float(data[57:63])*33.86 # hPa
 rain = float(data[66:72])*25.4 # mm
 
+# Try to create an annual folder to store the data in
+year = date.strftime("%Y")
+try:
+    os.system(f'mkdir {sdir}/{year}')
+except:
+    pass
+
 # Save the data
 # First try to write to an existing file
 try:
-    fnout = open(f"{sdir}/ValpoMetTower_{date.strftime('%Y%m%d')}.csv", 'r+')
+    fnout = open(f"{sdir}/{year}/ValpoMetTower_{date.strftime('%Y%m%d')}.csv", 'r+')
 
     # Check that not saving a duplicate time
     if (datetime.strptime(list(fnout)[-1].split(',')[0], "%Y-%m-%d_%H:%M:%S") == date):
         pass
     else:
-         fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{dewp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
+         fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
 
 except Exception as err: # Must be a new file
-    print(err)
+    print('WARNING', err)
 
-    fnout = open(f"{sdir}/ValpoMetTower_{date.strftime('%Y%m%d')}.csv", 'a+')
+    fnout = open(f"{sdir}/{year}/ValpoMetTower_{date.strftime('%Y%m%d')}.csv", 'a+')
 
     # Write header then data
-    fnout.write('Date (YYYY-MM-DD_HH:MM:SS local),Time (hours),Temp (C),Dewp (C),RH (%),Pres (mb),Rain (mm),Wspd (m/s),Wdir (deg),SWdown (W/m2)')
-    fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{dewp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
+    fnout.write('Date (YYYY-MM-DD_HH:MM:SS local),Time (hours),Temp (C),RH (%),Pres (mb),Rain (mm),Wspd (m/s),Wdir (deg),SWdown (W/m2)')
+    fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
 
 # Close the file
 fnout.close()
