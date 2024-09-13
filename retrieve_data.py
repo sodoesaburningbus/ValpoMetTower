14deb10c16119a4f45fc12c4761600ba973e4332
Christopher E. Phillips <cephillips574@gmail.com>
2024-09-13 11:48:22 -0500

    Update retrieve_data.py
    File data is actually data logger temperature, not dewp. Removed.

--- retrieve_data.py
+++ retrieve_data.py
@@ -22,7 +22,6 @@ date = datetime.strptime(data[:17], "%y/%m/%d %H:%M:%S") # Local
 wdir = float(data[18:21]) # deg
 wspd = float(data[22:27])*0.447 # m/s
 sdown = float(data[32:36]) # W/m2
-dewp = (float(data[38:43])-32.0)/1.8 # 'C
 temp = (float(data[45:50])-32.0)/1.8 # 'C
 rh = float(data[52:55]) # %
 pres = float(data[57:63])*33.86 # hPa
@@ -37,7 +36,7 @@ try:
     if (datetime.strptime(list(fnout)[-1].split(',')[0], "%Y-%m-%d_%H:%M:%S") == date):
         pass
     else:
-         fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{dewp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
+         fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
 
 except Exception as err: # Must be a new file
     print(err)
@@ -45,8 +44,8 @@ except Exception as err: # Must be a new file
     fnout = open(f"{sdir}/ValpoMetTower_{date.strftime('%Y%m%d')}.csv", 'a+')
 
     # Write header then data
-    fnout.write('Date (YYYY-MM-DD_HH:MM:SS local),Time (hours),Temp (C),Dewp (C),RH (%),Pres (mb),Rain (mm),Wspd (m/s),Wdir (deg),SWdown (W/m2)')
-    fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{dewp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
+    fnout.write('Date (YYYY-MM-DD_HH:MM:SS local),Time (hours),Temp (C),RH (%),Pres (mb),Rain (mm),Wspd (m/s),Wdir (deg),SWdown (W/m2)')
+    fnout.write(f'\n{date.strftime("%Y-%m-%d_%H:%M:%S")},{temp:.2f},{rh:.2f},{pres:.2f},{rain:.2f},{wspd:.2f},{wdir:.2f},{sdown:.2f}')
 
 # Close the file
 fnout.close()
